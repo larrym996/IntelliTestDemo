8c2c7b6a74d0ae93629b5cc9f9b8dc9fec91e31b
larrym996 <larrym996@gmail.com>
2019-01-15 19:47:05 -0500

    init

---
+++ IntelliTestDemo/IntelliTest/ClassLibrary1/ClassLibrary1/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        // convert ‘hello world’ to ‘HelloWorld’
+        // punctuation is turned into ‘_’, others are ignored.
+        public static string Capitalize(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value)) return value;
+
+            // WARNING: this sample is for demonstration only: it *contains* bugs.
+            var sb = new StringBuilder();
+            bool word = false;
+            foreach (var c in value)
+            {
+                if (char.IsLetter(c))
+                {
+                    if (word)
+                        sb.Append(c);
+                    else
+                    {
+                        sb.Append(char.ToUpper(c));
+                        word = true;
+                    }
+                }
+                else
+                {
+                    if (c == '!')
+                        sb.Append('_');
+                    word = false;
+                }
+            }
+            return sb.ToString();
+        }// Credit goes to Peli and Nickolai http://www.codeproject.com/Articles/31141/Getting-started-with-automated-white-box-testing-a
+    }
+}
---
+++ IntelliTestDemo/IntelliTest/Utilities/StringUtils.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities
+{
+    public class StringUtils
+    {
+        public static string Capitalize(string value)
+        {
+            // WARNING: this sample is for demonstration only: it *contains* bugs.
+            if (string.IsNullOrEmpty(value)) return value;
+
+            var sb = new StringBuilder();
+            bool word = false;
+            foreach (var c in value)
+            {
+                if (char.IsLetter(c))
+                {
+                    if (word)
+                        sb.Append(c);
+                    else
+                    {
+                        sb.Append(char.ToUpper(c));
+                        word = true;
+                    }
+                }
+                else
+                {
+                    if (c == '!')
+                sb.Append('_');
+                    word = false;
+                }
+            }
+            return sb.ToString();
+        }// Credit goes to Peli and Nikolai http://www.codeproject.com/Articles/31141/Getting-started-with-automated-white-box-testing-a
+    }
+}
